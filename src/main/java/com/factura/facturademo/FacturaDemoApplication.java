611e5fac8f19a684cca22a209843429351e3e8e5
Lucas Blandón P <lucasblandon@gmail.com>
2022-07-27 16:36:43 -0500

    first commit

---
+++ src/main/java/com/factura/facturademo/FacturaDemoApplication.java
@@ -0,0 +1,13 @@
+package com.factura.facturademo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FacturaDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FacturaDemoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/factura/facturademo/FacturaDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.factura.facturademo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FacturaDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
